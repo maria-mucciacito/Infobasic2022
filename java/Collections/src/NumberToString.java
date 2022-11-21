fe62d99deb30bb494a509ade0fbe83b8023dfea2
pc4 <mucciacitomaria@gmail.com>
2022-11-21 17:31:50 +0100

    .

--- java/Collections/src/NumberToString.java
+++ java/Collections/src/NumberToString.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 /*Exercise 1.4.6
 Progettare una classe`NumberToString`, con un campo `HashMap<Integer, String>`.\
 Nel costruttore della classe (che non prende parametri in input), la mappa viene inizializzata e vengono inseriti i valori come segue:
@@ -7,5 +9,30 @@ La mappa risultante sarà: `{1: “one”, 2: “two”, 3: “three”, 4: “f
 
 
 public class NumberToString {
-    
+    private Map<Integer,String> myHashMap;
+
+    public NumberToString() {
+        this.myHashMap.put(1, "zero");
+        this.myHashMap.put(2, "one");
+        this.myHashMap.put(3, "two");
+        this.myHashMap.put(4, "three");
+        this.myHashMap.put(5, "four");
+        this.myHashMap.put(6, "five");
+        this.myHashMap.put(7, "sex");
+        this.myHashMap.put(8, "seven");
+        this.myHashMap.put(9, "eight");
+        this.myHashMap.put(10, "nine");
+
+        
+    }
+
+
+    public Map<Integer,String> getMyHashMap() {
+        return this.myHashMap;
+    }
+
+    public void setMyHashMap(Map<Integer,String> myHashMap) {
+        this.myHashMap = myHashMap;
+    }
+
 }
